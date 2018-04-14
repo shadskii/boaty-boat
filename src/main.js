7882cc81b88ea944fa84ae28cb6ce4f0cb1a2f43
shadskii <jake.k.hassel@gmail.com>
2018-04-14 12:12:07 -0400

    Trying a different bootstrap

--- src/main.js
+++ src/main.js
@@ -1,57 +1,30 @@
-import 'pixi'
-import 'p2'
-import Phaser from 'phaser'
+import 'phaser';
+import BootScene from './BootScene';
+import MarioBrosScene from './MarioBrosScene';
+import TitleScene from './TitleScene';
 
-import BootState from './states/Boot'
-import SplashState from './states/Splash'
-import GameState from './states/Game'
 
-import config from './config'
-
-class Game extends Phaser.Game {
-  constructor () {
-    const docElement = document.documentElement
-    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
-    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
-
-    super(width, height, Phaser.CANVAS, 'content', null)
-
-    this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
-    this.state.add('Game', GameState, false)
-
-    // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
-    if (!window.cordova) {
-      this.state.start('Boot')
-    }
-  }
-}
-
-window.game = new Game()
-
-if (window.cordova) {
-  var app = {
-    initialize: function () {
-      document.addEventListener(
-        'deviceready',
-        this.onDeviceReady.bind(this),
-        false
-      )
+let config = {
+    type: Phaser.WEBGL,
+    parent: 'content',
+    width: 400,
+    height: 240,
+    scaleMode: 0, //Phaser.ScaleManager.EXACT_FIT,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 800 },
+            debug: false
+        }
     },
+    scene: [
+        BootScene,
+        TitleScene,
+        MarioBrosScene,
+    ]
+};
 
-    // deviceready Event Handler
-    //
-    onDeviceReady: function () {
-      this.receivedEvent('deviceready')
+let game = new Phaser.Game(config);
 
-      // When the device is ready, start Phaser Boot state.
-      window.game.state.start('Boot')
-    },
-
-    receivedEvent: function (id) {
-      console.log('Received Event: ' + id)
-    }
-  }
-
-  app.initialize()
-}
+/*
+https://codepen.io/samme/pen/JMVBeV*/
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,13 +5,13 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
-var pixi = path.join(phaserModule, 'build/custom/pixi.js')
-var p2 = path.join(phaserModule, 'build/custom/p2.js')
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'src/phaser.js')
 
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false')),
+  WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
+  CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
 })
 
 module.exports = {
@@ -20,7 +20,7 @@ module.exports = {
       'babel-polyfill',
       path.resolve(__dirname, 'src/main.js')
     ],
-    vendor: ['pixi', 'p2', 'phaser', 'webfontloader']
+    //vendor: ['pixi']
 
   },
   output: {
@@ -32,14 +32,14 @@ module.exports = {
     definePlugin,
     new CleanWebpackPlugin(['build']),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
+    /*new webpack.optimize.UglifyJsPlugin({
       drop_console: true,
       minimize: true,
       output: {
         comments: false
       }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' /* chunkName= */ , filename: 'js/vendor.bundle.js' /* filename= */ }),
+    }),*/
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' /* chunkName= */, filename: 'js/vendor.bundle.js' /* filename= */ }),
     new HtmlWebpackPlugin({
       filename: 'index.html', // path.resolve(__dirname, 'build', 'index.html'),
       template: './src/index.html',
@@ -64,9 +64,8 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-      { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
-      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] }
+      { test: /phaser-split\.js$/, use: 'raw-loader' },
+      { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
     ]
   },
   node: {
@@ -77,8 +76,7 @@ module.exports = {
   resolve: {
     alias: {
       'phaser': phaser,
-      'pixi': pixi,
-      'p2': p2
+
     }
   }
 }
