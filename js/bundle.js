6b41b8ec821dd7778c26469bfdd0a6a486457b89
shadskii <jake.k.hassel@gmail.com>
2018-04-15 20:48:17 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -147235,10 +147235,10 @@ var BootScene = function (_Phaser$Scene) {
     _createClass(BootScene, [{
         key: 'preload',
         value: function preload() {
-            this.load.image('sky', '../assets/images/sky.png');
-            this.load.image('ground', '../assets/images/platform.png');
-            this.load.image('mine', '../assets/images/sea_mine.png');
-            this.load.image('boaty', '../assets/images/boaty.png');
+            this.load.image('sky', 'assets/images/sky.png');
+            this.load.image('ground', 'assets/images/platform.png');
+            this.load.image('mine', 'assets/images/sea_mine.png');
+            this.load.image('boaty', 'assets/images/boaty.png');
         }
     }, {
         key: 'create',
