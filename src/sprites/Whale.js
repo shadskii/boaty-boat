1d207db88a3c2b004b03fc35d7b8e80a2a38c1a4
shadskii <jake.k.hassel@gmail.com>
2018-04-30 22:45:40 -0400

    The whale is more reasonably sized

--- src/sprites/Whale.js
+++ src/sprites/Whale.js
@@ -7,6 +7,6 @@ export default class Whale extends Enemy {
         this.acceleration = 0;
         this.body.maxVelocity.y = 0;
         this.body.setSize(200, 20);
-        this.setScale(0.2, 0.2);
+        this.setScale(0.17, 0.17);
     }
 }
