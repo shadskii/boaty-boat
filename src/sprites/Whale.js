89ef068304a6035c6385ca052e9b370b30ab9ac7
shadskii <jake.k.hassel@gmail.com>
2018-05-28 11:35:42 -0400

    Resized whale

--- src/sprites/Whale.js
+++ src/sprites/Whale.js
@@ -7,6 +7,6 @@ export default class Whale extends Enemy {
         this.acceleration = 0;
         this.body.maxVelocity.y = 0;
         this.body.setSize(200, 20);
-        this.setScale(0.1, 0.1);
+        this.setScale(0.5, 0.5);
     }
 }
